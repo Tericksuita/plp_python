58407996500cfe1f723d66f42c7e4b243dc1cb74
Tericksuita <erickmuhoro200@gmail.com>
2024-02-22 20:56:44 +0300

    assignment 1

---
+++ greetings.py
@@ -0,0 +1,2 @@
+name = input('What shall i call you master :')
+print('Hello Master ' + name)
---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+location =input('Where do you live? ')
+print('Top of the morning dear ' + name + ', must be nice being ' + age + 'years old in ' + location)
